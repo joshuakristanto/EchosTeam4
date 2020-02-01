75f322c92e98196bd402ffc291e2b60a4fe5fa42
Christian <christian.v.t.shadd@gmail.com>
2020-02-01 15:14:11 -0800

    Stable opus output, untested

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const fs = require('fs-extra');
 const path = require('path');
-const { Readable } = require('stream');
 // Configuration file that must be in JSON format
 const { prefix, token } = require('./config.json');
 
@@ -15,55 +14,43 @@ var chatChannel = null;
 // Voice channel cache
 var voiceChannel = null;
 
-var podcasts = new Map();
+var radios = new Map();
 
-var writeStream = null;
-
-class Silence extends Readable {
-    _read() {
-      this.push(Buffer.from([0xF8, 0xFF, 0xFE]));
-    }
-  }
-
-function startRecording(member, podcast, msg) {
-    const connection = podcast.voiceConnection;
-    const receiver = connection.createReceiver();
-    receiver.on('pcm', () => { console.log('a'); });
+async function startRecording(member, radio) {
+    const connection = radio.connection;
+    const receiver = connection.receiver;
     connection.on('speaking', (user, speaking) => {
         if (speaking) {
             console.log('1');
         }
-        if (!speaking) {
-            console.log('2');
-        }
     })
-    const voiceStream = receiver.createPCMStream(member);
-    const output = path.join(podcast.output, `${member.id}-${Date.now()}.raw_pcm`);
+    const voiceStream = receiver.createStream(member, { mode: 'opus', end: 'manual' });
+    const output = path.join(radio.output, `${member.id}-${Date.now()}.raw_opus`);
     const writeStream = fs.createWriteStream(output);
     voiceStream.pipe(writeStream);
-    podcast.members.set(member.id, {
+    radio.members.set(member.id, {
         voiceStream,
         writeStream
     });
     voiceStream.on('close', () => {
         console.log('end of recording voice!');
-        podcast.members.delete(member.id);
+        radio.members.delete(member.id);
         writeStream.end(err => {
             if (err) {
-                log.error(err);
+                console.log(err);
             }
         });
     });
 }
 
-function stopRecording(member, podcast) {
-    const memberData = podcast.members.get(member.id);
+async function stopRecording(member, radio) {
+    const memberData = radio.members.get(member.id);
     if (memberData) {
         const { voiceStream, writeStream } = memberData;
         voiceStream.destroy();
         writeStream.end();
     }
-    podcast.members.delete(member.id);
+    radio.members.delete(member.id);
 }
 
 // Ready event
@@ -82,58 +69,58 @@ client.on('message', async (msg) => {
             msg.reply('Pong!');
         }
         else if (command === 'activate') { // Activate recording command
-            if (!voiceChannel) {
-                voiceChannel = member.voiceChannel;
-                if (args[0]) {
-                    chatChannel = client.channels.find(channel => channel.name === args[0]);
-                    if (voiceChannel && chatChannel) {
-                        const output = path.join(outputPath, `${voiceChannel.channelID}-${Date.now()}`);
-                            
-                        const [voiceConnection] = await Promise.all([
-                            voiceChannel.join(),
-                            fs.ensureDir(output)
-                        ]);
-                        if (voiceConnection) {
-                            msg.reply(`Ready to transmit in ${chatChannel.name}!`);
-    
-                            const podcast = {
-                                name: voiceChannel.name,
-                                output,
-                                voiceConnection,
-                                members: new Map()
-                            };
-                            voiceConnection.on('ready', ()=>{
-                                console.log("test");
-                                voiceConnection.play(new Silence(), { type: 'opus' });
-                                })
-                            podcasts.set(voiceChannel.channelID, podcast);
-                            voiceChannel.members.forEach((member) => {
-                                startRecording(member, podcast, msg);
-                            });
+            if (args[0]) {
+                chatChannel = client.channels.find(channel => channel.name === args[0]);
+                if (chatChannel) {
+                    if (!voiceChannel) {
+                        const voice = member.voice;
+                        if (voice) {
+                            voiceChannel = voice.channel;
+                            if (voiceChannel) {
+                                const output = path.join(outputPath, `${voiceChannel.channelID}-${Date.now()}`);
+                                fs.ensureDir(output);
+                                voiceChannel.join().then(connection => {
+                                    msg.reply(`Ready to transmit in ${chatChannel.name}!`);
+            
+                                    const radio = {
+                                        name: voiceChannel.name,
+                                        output,
+                                        connection,
+                                        members: new Map()
+                                    };
+                                    radios.set(voiceChannel.channelID, radio);
+                                    voiceChannel.members.forEach((member) => {
+                                        startRecording(member, radio);
+                                    });
+                                });
+                            }
+                            else {
+                                msg.reply('Could not use the voice channel!');
+                            }
                         }
                         else {
-                            msg.reply('Could not use the voice channel!');
+                            msg.reply('Could not get the voice state!');
                         }
                     }
                     else {
-                        msg.reply('Could not use the voice channel or chat channel!');
+                        msg.reply('Voice channel was already specified, please deactivate!');
                     }
                 }
                 else {
-                    msg.reply('Please specify a channel!');
+                    msg.reply('Could not use the chat channel!');
                 }
             }
             else {
-                msg.reply('Voice channel was already specified, please deactivate!');
+                msg.reply('Please specify a channel!');
             }
         }
         else if (command === 'deactivate') { // Deactivate recording command
             chatChannel = null;
             if (voiceChannel) {
                 msg.reply('No longer ready to transmit!');
-                const podcast = podcasts.get(voiceChannel.channelID);
-                podcast.members.forEach((member) => {
-                    stopRecording(member, podcast);
+                const radio = radios.get(voiceChannel.channelID);
+                radio.members.forEach((member) => {
+                    stopRecording(member, radio);
                 });
                 voiceChannel.leave();
             }
