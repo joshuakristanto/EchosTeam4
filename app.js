90ab94abb9a1778152ad26a5b1bbf00b8b15e606
Christian <christian.v.t.shadd@gmail.com>
2020-02-02 09:47:00 -0800

    Added comments

--- app.js
+++ app.js
@@ -8,18 +8,22 @@ const path = require('path');
 const { Readable, Transform } = require('stream');
 
 // Configuration file that must be in JSON format
+// with a prefix, outputSocketPort, and your token
 const { prefix, outputSocketPort, token } = require('./config.json');
 
 // Client
 const client = new Discord.Client();
-// Google Speech Client for Speech to Text
+// Google Speech to Text which requires you to set up a Google API key
+// in your PATH system variables
 const googleSpeechClient = new googleSpeech.SpeechClient();
-// Output Path
+// Output path
 const outputPath = path.join(__dirname, 'output');
-// This is a hack that sent a silence frame so that we can keep the bot on
+// Silent frame
 const silenceFrame = Buffer.from([0xF8, 0xFF, 0xFE]);
+// Streaming text limit
 const streamingLimit = 290000;
 
+// Bridging text offset
 var bridgingOffset = 0;
 // Chat channel cache
 var chatChannel = null;
@@ -27,14 +31,18 @@ var chatChannel = null;
 var voiceChannel = null;
 // Radio cache
 var radios = new Map();
+// Resulting end time for text
 var resultEndTime = 0;
 
+// Silence object class for a silent frame to keep bot alive
 class Silence extends Readable {
     _read() {
         this.push(silenceFrame);
     }
 }
 
+// Converts from n-channel to 1 channel for audio processing for
+// compatbility for Google
 class ConvertTo1ChannelStream extends Transform {
     constructor(source, options) {
       super(options)
@@ -44,6 +52,9 @@ class ConvertTo1ChannelStream extends Transform {
     }
 }
 
+// Converts from n-buffer to 1 channel for audio processing for
+// compatibility for Google
+/** @return {Buffer} */
 function convertBufferTo1Channel(buffer) {
     const convertedBuffer = Buffer.alloc(buffer.length / 2);
   
@@ -55,12 +66,17 @@ function convertBufferTo1Channel(buffer) {
     return convertedBuffer;
 }
 
+// Recording function for streaming
 function startRecording(member, radio) {
+    // Connection to voice channel
     const connection = radio.connection;
+    // Voice reciever for the connection
     const receiver = connection.receiver;
 
+    // Create a stream for the voice reciever of the voice channel
     const voiceStream = receiver.createStream(member, { mode: 'pcm' });
 
+    // Configiuration for streaming for proper audio quality
     const requestConfig = {
         encoding: 'LINEAR16',
         sampleRateHertz: 48000,
@@ -90,7 +106,7 @@ function startRecording(member, radio) {
             if (stream.results[0].isFinal) {
                 // process.stdout.write(`${stdoutText}\n`);
                 let transcript = stream.results[0].alternatives[0].transcript;
-                //send data to socket client
+                // Send data to socket client
                 console.log(`${member.tag}: ${transcript}`);
                 io.emit('message', `${member.tag}: ${transcript}`);
                 radio.chatChannel.send(`${member.tag}: ${transcript}`);
@@ -116,7 +132,10 @@ function startRecording(member, radio) {
             radio.chatChannel.send(`${member.tag}: ${transcription}`);
     });*/
 
+    // Conversion of stream for compatibility
     const convertTo1ChannelStream = new ConvertTo1ChannelStream();
+
+    // Piping stream to functions and outputs
     voiceStream.pipe(convertTo1ChannelStream).pipe(recognizeStream);
 
     radio.members.set(member.id, {
@@ -139,6 +158,7 @@ function startRecording(member, radio) {
     }); */
 }
 
+// Stop the recording of the stream
 function stopRecording(member, radio) {
     const memberData = radio.members.get(member.id);
     if (memberData) {
@@ -161,29 +181,45 @@ client.on('ready', async () => {
 // Message event
 client.on('message', async (msg) => {
     if (msg.content.startsWith(prefix) && !msg.author.bot) {
-        const args = msg.content.slice(prefix.length).split(' '); // Arguments of a message
-        const command = args.shift().toLowerCase(); // First element of message
-        const member = msg.member; // Member who specified the message
+        // Arguments of a message so that message is split up
+        const args = msg.content.slice(prefix.length).split(' ');
+        // First element of message which is normally the command
+        const command = args.shift().toLowerCase();
+        // Member who specified the command
+        const member = msg.member;
                         
-        if (command === 'ping') { // Test command
+        // Test command
+        if (command === 'ping') {
             msg.reply('Pong!');
             console.log('Was pinged!');
         }
-        else if (command === 'activate') { // Activate recording command
+        // Activate recording command
+        else if (command === 'activate') {
             if (args[0]) {
+                // This gets the chat channel specified from the arguments of the activate
+                // command
                 chatChannel = client.channels.find(channel => channel.name === args[0]);
                 if (chatChannel) {
+                    // The bot must not be in an existing voice channel
                     if (!voiceChannel) {
+                        // Voice state object of the member
                         const voice = member.voice;
                         if (voice) {
+                            // Voice channel the member is in
                             voiceChannel = voice.channel;
+                            // The member must be in an existing voice channel
                             if (voiceChannel) {
+                                // This is for an output folder for streaming files
                                 const output = path.join(outputPath, `${voiceChannel.channelID}-${Date.now()}`);
                                 fs.ensureDir(output);
+                                
+                                // Connection processing
                                 const connection = await voiceChannel.join().then(async connection =>{
                                     msg.reply(`Ready to transmit in ${chatChannel.name}!`);
                                     console.log('Activated!');
+                                    // Silent frame to get bot working
                                     connection.play(new Silence(), { type: 'opus' });
+                                    // Speaking event
                                     connection.on('speaking', (user, speaking) => {
                                         if (user) {
                                             if (speaking) {
@@ -196,11 +232,14 @@ client.on('message', async (msg) => {
                                                     members: new Map()
                                                 };
                                                 radios.set(voiceChannel.channelID, radio);
+
+                                                // The user speaking should not be the bot
                                                 if (user.id != client.user.id) {
                                                     startRecording(user, radio);
                                                 }
                                             }
                                             else {
+                                                // Silent frame when there is no activity to keep alive
                                                 connection.play(new Silence(), { type: 'opus' });
                                             }
                                         }
@@ -227,7 +266,8 @@ client.on('message', async (msg) => {
                 msg.reply('Please specify a channel!');
             }
         }
-        /*else if (command === 'deactivate') { // Deactivate recording command
+        // Deactivate recording command
+        /*else if (command === 'deactivate') {
             chatChannel = null;
             if (voiceChannel) {
                 msg.reply('No longer ready to transmit!');
@@ -243,8 +283,10 @@ client.on('message', async (msg) => {
     }
 });
 
+// Listen on specified socket port to send the bot messages to said port
 http.listen(outputSocketPort, function () {
     console.log(`Output listening on *:${outputSocketPort}`);
 });
 
+// Login to Discord bot
 client.login(token);
