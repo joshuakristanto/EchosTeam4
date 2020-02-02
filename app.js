3fb3a6caf7c985f23c512b4dee1c8a01ec0b9a04
Christian Shadd <christian.v.t.shadd@gmail.com>
2020-02-01 23:30:17 -0800

    final product?

--- app.js
+++ app.js
@@ -35,26 +35,6 @@ class Silence extends Readable {
     }
 }
 
-class ConvertTo1ChannelStream extends Transform {
-    constructor(source, options) {
-      super(options)
-    }
-    _transform(data, encoding, next) {
-      next(null, convertBufferTo1Channel(data))
-    }
-}
-
-function convertBufferTo1Channel(buffer) {
-    const convertedBuffer = Buffer.alloc(buffer.length / 2);
-  
-    for (let i = 0; i < convertedBuffer.length / 2; i++) {
-      const uint16 = buffer.readUInt16LE(i * 4);
-      convertedBuffer.writeUInt16LE(uint16, i * 2);
-    }
-  
-    return convertedBuffer;
-}
-
 function startRecording(member, radio) {
     const connection = radio.connection;
     const receiver = connection.receiver;
